d7159b57b53adf32728a88a81aa41167282bf129
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2025-04-28 16:01:03 -0500

    feat: :tada: Initial Project Structure
    Skeleton Project Spring

---
+++ src/main/java/com/inventariotibu/inv_tibu/InvTibuApplication.java
@@ -0,0 +1,13 @@
+package com.inventariotibu.inv_tibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvTibuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvTibuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventariotibu/inv_tibu/InvTibuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventariotibu.inv_tibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvTibuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
